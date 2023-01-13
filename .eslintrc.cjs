385c23bb9755bd565445bdb7b974541530c9e1d0
Pavel Mareev <pavel.mareev@aliexpress.ru>
2023-01-13 16:27:50 +0500

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
